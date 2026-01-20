8fb640f8bd471dfc4cf758925049417e731b67c5
mokwathedeveloper <moffatmokwa12@gmail.com>
2026-01-20 22:42:24 +0300

    Fix middleware server error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,10 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware((auth, req) => {
-  // Middleware processing
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Exclude files in /public folder
     '/((?!_next|[^?]*\.(?:css|js|png|jpg|jpeg|svg|gif|webp)$).*)',
-    // Exclude /api/auth routes
     '/(api|trpc)(.*)',
   ],
 };
